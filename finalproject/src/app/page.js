00a7d687ac9035f0a38dd02e2ac6faf52dc212d5
kushal-5 <prohackie3@gmail.com>
2025-10-22 23:54:25 +0545

    navbar

--- finalproject/src/app/page.js
+++ finalproject/src/app/page.js
@@ -3,7 +3,14 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h1>Hello World 123</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+              <li>Login</li>
+          <li>Register</li>
+        </ul>
+      </nav>
+      <h1>Hello World 12323</h1>
       <p>This is a paragraph</p>
       <button>Click me</button>
       <input type="text" placeholder="Enter your name" />
@@ -13,6 +20,7 @@ const page = () => {
         <option value="3">Option 3</option>
       </select>
     </div>
+    
 
 
   )
